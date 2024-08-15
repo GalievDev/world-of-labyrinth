778107972551396f65402ca43060185c81efe186
galiev <ggvalion@gmail.com>
2024-08-15 12:52:07 +0500

    Custom biome added

---
+++ src/main/kotlin/dev/galiev/worldoflabyrinth/world/biome/BiomeRegistry.kt
@@ -0,0 +1,12 @@
+package dev.galiev.worldoflabyrinth.world.biome
+
+import dev.galiev.worldoflabyrinth.WorldOfLabyrinth.MOD_ID
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.util.Identifier
+import net.minecraft.world.biome.Biome
+
+object BiomeRegistry {
+    val LABYRINTH_BIOME: RegistryKey<Biome> = RegistryKey.of(RegistryKeys.BIOME,
+        Identifier.of(MOD_ID, "labyrinth_biome"))
+}
