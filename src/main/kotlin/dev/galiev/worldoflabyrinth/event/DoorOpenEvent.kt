4b374b91e36c3ed58926e36c8dc0c917435837d6
galiev <ggvalion@gmail.com>
2024-08-18 18:57:13 +0500

    Event modified

--- src/main/kotlin/dev/galiev/worldoflabyrinth/event/DoorOpenEvent.kt
+++ src/main/kotlin/dev/galiev/worldoflabyrinth/event/DoorOpenEvent.kt
@@ -3,12 +3,14 @@ package dev.galiev.worldoflabyrinth.event
 import dev.galiev.worldoflabyrinth.world.dimension.DimensionRegistry
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.minecraft.block.Blocks
+import net.minecraft.block.ChestBlock
+import net.minecraft.entity.EntityType
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
-import net.minecraft.entity.mob.ZombieEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Items
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Formatting
@@ -18,25 +20,29 @@ import net.minecraft.world.World
 
 object DoorOpenEvent: UseBlockCallback {
     override fun interact(player: PlayerEntity?, world: World?, hand: Hand?, hitResult: BlockHitResult?): ActionResult {
-
-        if (player?.world?.registryKey == DimensionRegistry.WOL_LEVEL_KEY) {
+        if (world?.isClient!!) { return ActionResult.PASS }
+        if (player?.world?.registryKey == DimensionRegistry.WOL_LEVEL_KEY && world is ServerWorld) {
             if (player.isCreative) {
                 return ActionResult.PASS
             }
-            val block = world?.getBlockState(hitResult?.blockPos)?.block
+            val block = world.getBlockState(hitResult?.blockPos)?.block
             if (block == Blocks.SPRUCE_DOOR) {
                 player.addStatusEffect(StatusEffectInstance(StatusEffects.BLINDNESS, 200, 3))
                 player.addStatusEffect(StatusEffectInstance(StatusEffects.SLOWNESS, 200, 3))
                 player.sendMessage(Text.literal("HAHAHA did you seriously think it was that easy?").formatted(Formatting.RED), true)
-                val zombie = ZombieEntity(world)
-                world?.spawnEntity(zombie)
-                zombie.equipStack(EquipmentSlot.HEAD, Items.DIAMOND_HELMET.defaultStack)
-                zombie.equipStack(EquipmentSlot.BODY, Items.DIAMOND_CHESTPLATE.defaultStack)
-                zombie.equipStack(EquipmentSlot.LEGS, Items.DIAMOND_LEGGINGS.defaultStack)
-                zombie.equipStack(EquipmentSlot.FEET, Items.DIAMOND_BOOTS.defaultStack)
-                zombie.equipStack(EquipmentSlot.MAINHAND, Items.DIAMOND_SWORD.defaultStack)
+                val zombie = EntityType.ZOMBIE.create(world)
+                zombie?.equipStack(EquipmentSlot.HEAD, Items.DIAMOND_HELMET.defaultStack)
+                zombie?.equipStack(EquipmentSlot.CHEST, Items.DIAMOND_CHESTPLATE.defaultStack)
+                zombie?.equipStack(EquipmentSlot.LEGS, Items.DIAMOND_LEGGINGS.defaultStack)
+                zombie?.equipStack(EquipmentSlot.FEET, Items.DIAMOND_BOOTS.defaultStack)
+                zombie?.equipStack(EquipmentSlot.MAINHAND, Items.DIAMOND_SWORD.defaultStack)
+                zombie?.setPosition(player.x - 1, player.y, player.z - 1)
+
+                world.spawnEntity(zombie)
 
                 return ActionResult.FAIL
+            } else if (block is ChestBlock) {
+                return ActionResult.PASS
             }
             return ActionResult.FAIL
         }
