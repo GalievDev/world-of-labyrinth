6b21ee282acc3cdfcd74a9cf25f4a1b2e2e022bd
Vazir Vadirov <ggvalion@gmail.com>
2025-01-26 14:37:24 +0500

    Added using sound and changing texture

--- src/main/kotlin/dev/galiev/worldoflabyrinth/item/GuideStone.kt
+++ src/main/kotlin/dev/galiev/worldoflabyrinth/item/GuideStone.kt
@@ -1,19 +1,29 @@
 package dev.galiev.worldoflabyrinth.item
 
+import dev.galiev.worldoflabyrinth.component.DataComponentType
+import net.minecraft.entity.Entity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.registry.tag.StructureTags
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 
-class Pendulum : Item(Settings().maxCount(1)) {
+class GuideStone : Item(Settings().maxCount(1)) {
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        val stack = user.getStackInHand(hand)
+
+        world.playSound(user, user.blockPos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS)
+
+        user.itemCooldownManager.set(this, 240)
+
         if (world is ServerWorld) {
             val structurePos = world.locateStructure(StructureTags.EYE_OF_ENDER_LOCATED, user.blockPos, 100, false)!!
             val direction = Vec3d(
@@ -22,12 +32,12 @@ class Pendulum : Item(Settings().maxCount(1)) {
                 (structurePos.z + 0.5) - user.z
             ).normalize()
 
-            for (i in 1..160) {
+            for (i in 1..20) {
                 val offset = direction.multiply(i * 0.5)
                 val particlePos = user.pos.add(offset)
 
                 world.spawnParticles(
-                    ParticleTypes.END_ROD,
+                    ParticleTypes.FLAME,
                     particlePos.x,
                     particlePos.y + 1,
                     particlePos.z,
@@ -39,6 +49,17 @@ class Pendulum : Item(Settings().maxCount(1)) {
                 )
             }
         }
-        return super.use(world, user, hand)
+
+        return TypedActionResult.success(stack, world.isClient)
+    }
+
+    override fun inventoryTick(stack: ItemStack, world: World, entity: Entity, slot: Int, selected: Boolean) {
+        if (entity is PlayerEntity) {
+            if (entity.itemCooldownManager.isCoolingDown(this)) {
+                stack.set(DataComponentType.ACTIVATED, true)
+            } else {
+                stack.set(DataComponentType.ACTIVATED, false)
+            }
+        }
     }
 }
