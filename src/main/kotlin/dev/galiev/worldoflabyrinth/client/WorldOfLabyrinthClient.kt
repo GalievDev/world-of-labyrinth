db102f8a63cbc44bf81db2fed5ffc2d9df120143
Vazir Vadirov <ggvalion@gmail.com>
2025-01-26 14:37:32 +0500

    Added using sound and changing texture

--- src/main/kotlin/dev/galiev/worldoflabyrinth/client/WorldOfLabyrinthClient.kt
+++ src/main/kotlin/dev/galiev/worldoflabyrinth/client/WorldOfLabyrinthClient.kt
@@ -1,9 +1,12 @@
 package dev.galiev.worldoflabyrinth.client
 
+import dev.galiev.worldoflabyrinth.util.ModelPredicate
 import net.fabricmc.api.ClientModInitializer
 
 class WorldOfLabyrinthClient : ClientModInitializer {
 
     override fun onInitializeClient() {
+        ModelPredicate
+        //BuiltinItemRendererRegistry.INSTANCE.register(ItemRegistry.LABYRINTH_ORB, LabyrinthOrbRenderer())
     }
 }
